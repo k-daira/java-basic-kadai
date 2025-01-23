b034cc9509d7ac253e2a84c073b00350e771e5ab
k-daira <12amj0d6@au.com>
2025-01-24 01:22:58 +0900

    課題を追加

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,13 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+
+		Car_Chapter15 car = new Car_Chapter15();
+		car.run();
+		car.gearChange(3);
+		car.run();
+
+	}
+}
---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,25 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+
+	private int gear = 1;
+	private int speed = 10;
+
+	public void gearChange(int afterGear) {
+		switch (afterGear) {
+		case 1 -> speed = 10;
+		case 2 -> speed = 20;
+		case 3 -> speed = 30;
+		case 4 -> speed = 40;
+		case 5 -> speed = 50;
+		default -> speed = 10;
+		}
+
+		gear = afterGear;
+
+	}
+
+	public void run() {
+		System.out.println("時速" + speed + "km/s");
+	}
+}
